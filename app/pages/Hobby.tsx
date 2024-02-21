c7c35ebd2fe5fee7cf4d818d92083b10ca388765
Steven Ho <steven09ho@gmail.com>
2024-02-20 17:58:24 -0800

    added basic div for each page

---
+++ app/pages/Hobby.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// https://www.material-tailwind.com/docs/react/img
+// ^ Website to reference for image placement for hobbies
+
+export default function Hobby() {
+
+  return (
+    <div />
+  );
+}
---
+++ app/pages/TabNav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TabNav() {
+
+  return (
+    <div className="flex ..." style={{ background: "grey"}}>
+      <div className="flex-1 ...">01</div>
+      <div className="contents">
+        <div className="flex-1 ...">02</div>
+        <div className="flex-1 ...">03</div>
+      </div>
+      <div className="flex-1 ...">04</div>
+    </div>
+  );
+}
