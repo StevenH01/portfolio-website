ae761eeb3e4e5035d4033bb8fca7368daf1cec50
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-07 13:10:28 -0800

    fixed apostophy issues

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -1,8 +1,8 @@
 "use client" // this is a client component
-import React from "react"
-import Image from "next/image"
-import { Link } from "react-scroll/modules"
-import { HiArrowDown } from "react-icons/hi"
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
 
 const HomeSection = () => {
   return (
@@ -18,9 +18,9 @@ const HomeSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I'm Steven!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&apos;m Steven!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I'm a{" "}
+            I&apos;m a{" "}
             <span className="font-semibold text-teal-600">
               Software Engineer{" "}
             </span>
@@ -54,7 +54,7 @@ const HomeSection = () => {
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default HomeSection;
