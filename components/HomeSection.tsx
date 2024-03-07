4f1de001392f24bc6d244e594a1505472005118b
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-07 13:11:33 -0800

    changed apostrophy

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -18,9 +18,9 @@ const HomeSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&apos;m Steven!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Steven!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&apos;m a{" "}
+            I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
               Software Engineer{" "}
             </span>
